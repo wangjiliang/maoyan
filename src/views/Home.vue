3401ecad8f5084482d589f23c49c1694e081a244
wangjiliang <wang_jiliang@outlook.com>
2019-09-18 15:32:53 +0800

    修改初始目录

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    {{msg}}
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      msg: 'Home 组件'
+    }
   }
 }
 </script>
+
+<style>
+
+</style>
